d8c7ba8f8cddc99c4a352cbd046dae57fdcf06ea
Pavlenko Eugene <pavlenko.obs@gmail.com>
2025-03-03 15:09:19 +0200

    fix build

--- inc/stm32/clock.hpp
+++ inc/stm32/clock.hpp
@@ -1,3 +1,6 @@
 #if defined(STM32F1)
 #include <stm32/f1/clock.hpp>
+#elif defined(STM32F4)
+#include <stm32/f4/clock.hpp>
 #endif
+//TODO F4 missing
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
+#include <stm32/clock.hpp>
+
 /**
  * @brief Dummy main function
  */
 int main(void)
-{}
+{
+    using namespace STM32;
+
+    while (true)
+    {
+        asm volatile ("nop");
+    }
+    
+}
