21f1ebe838cacae52e9f2600a652217ffcdc2a02
Pavlenko Eugene <pavlenko.obs@gmail.com>
2025-10-06 20:13:33 +0300

    add i2c func

--- inc/stm32/dev/common/i2c_definitions.hpp
+++ inc/stm32/dev/common/i2c_definitions.hpp
@@ -10,12 +10,47 @@ namespace STM32::I2C
     private:
         static inline I2C_TypeDef *_regs();
 
+        static inline uint16_t devAddress{0};
+        static inline uint16_t ownAddress{0};
+
     public:
         using DMATx = tDMATx;
         using DMARx = tDMARx;
 
         static inline void configure();
 
-        // TODO sendDevAddress(); sendRegAddress(); send(addr...); recv(addr...); wait(ev);
+        // Example sync flow
+        // wait busy
+        // - timeout error if any
+        // enable ack
+        // send start
+        // - check success
+        // send dev address
+        // - check success
+        // send reg address (if needed)
+        // - check success
+        // send data by byte
+        // - check success
+        // disable ack
+        // send stop
+
+        /**
+         * @brief Listen address requests (automatically go to slave mode)
+         */
+        static inline void listen(uint16_t ownAddress);
+
+        /**
+         * @brief Select slave device for communicate (automatically go to master mode)
+         */
+        static inline void select(uint16_t devAddress);
+
+        static void send(uint8_t *data, uint16_t size);
+        static void recv(uint8_t *data, uint16_t size);
+        static void memSet(uint16_t reg, uint8_t *data, uint16_t size);
+        static void memGet(uint16_t reg, uint8_t *data, uint16_t size);
+        static bool isBusy();
+
+    protected:
+        //TODO helper functions: start/stop; send dev addr; send reg addr; wait; busy check; service via irq, data via dma, state!!!
     };
 }
