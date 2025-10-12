c7b1bb2c48b0ce46879a7d07a9525313c0f09856
Pavlenko Eugene <pavlenko.obs@gmail.com>
2025-10-12 19:40:41 +0300

    add i2c pin info

--- inc/stm32/dev/f1/i2c.hpp
+++ inc/stm32/dev/f1/i2c.hpp
@@ -13,6 +13,9 @@
 
 namespace STM32
 {
+    // SCL: PB6 (PB8); SDA: PB7 (PB9); SMBA: PB5
     using I2C1 = I2C::Driver<I2C1_BASE, I2C1_EV_IRQn, I2C1_ER_IRQn, Clock::I2C1Clock, DMA::DMA1Channel6, DMA::DMA1Channel7>;
+
+    // SCL: PB10; SDA: PB11; SMBA: PB12
     using I2C2 = I2C::Driver<I2C2_BASE, I2C2_EV_IRQn, I2C2_ER_IRQn, Clock::I2C2Clock, DMA::DMA1Channel4, DMA::DMA1Channel5>;
 }
--- inc/stm32/dev/f4/i2c.hpp
+++ inc/stm32/dev/f4/i2c.hpp
@@ -16,9 +16,14 @@
 
 namespace STM32
 {
+    // SCL: PB6 (PB8); SDA: PB7 (PB9); SMBA: PB5
     using I2C1 = I2C::Driver<I2C1_BASE, I2C1_EV_IRQn, I2C1_ER_IRQn, Clock::I2C1Clock, DMA::DMA1Stream6Channel1, DMA::DMA1Stream0Channel1>;
+
+    // SCL: PB10; SDA: PB11; SMBA: PB12
     using I2C2 = I2C::Driver<I2C2_BASE, I2C2_EV_IRQn, I2C2_ER_IRQn, Clock::I2C2Clock, DMA::DMA1Stream7Channel7, DMA::DMA1Stream2Channel7>;
+
 #ifdef I2C3_BASE
+    // SCL: PA8; SDA: PC9; SMBA: PA9
     using I2C3 = I2C::Driver<I2C3_BASE, I2C3_EV_IRQn, I2C3_ER_IRQn, Clock::I2C3Clock, DMA::DMA1Stream4Channel3, DMA::DMA1Stream2Channel3>;
 #endif
 }
